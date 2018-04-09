0cabb31f2bb1a6704ab2fcd883510ab6b1c76186
Tien Ho <thuytien140894@gmail.com>
2018-04-09 11:02:25 -0700

    Edited the main program to evaluate ARITH.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,9 @@
 module Main where
 
   import Evaluator
+  import Syntax
 
   main :: IO ()
-  main = putStrLn "Hello World"
+  main = do 
+    print $ eval (Num 5)
+    print $ eval (Num 10 `Add` Num 20)
