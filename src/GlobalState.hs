19a95fddbac185c2a978782c5617c8dee713c8f9
Tien Ho <thuytien140894@gmail.com>
2018-04-16 12:19:19 -0700

    Added definition for an empty state.

--- src/GlobalState.hs
+++ src/GlobalState.hs
@@ -1,11 +1,19 @@
+{-# Language PatternSynonyms #-}
+{-# Language ViewPatterns #-}
+
 module GlobalState where 
 
   import Syntax 
 
-  import Data.Map (Map)
+  import Data.Map (Map, toList)
   import qualified Data.Map as Map
 
   newtype State = State (Map String Int)
+    deriving (Eq, Show)
+
+  pattern EmptyState :: State 
+  pattern EmptyState <- State (Map.null -> True) 
+    where EmptyState = State Map.empty
 
   lookUp :: State -> String -> Maybe Int 
   lookUp (State s) x = Map.lookup x s
