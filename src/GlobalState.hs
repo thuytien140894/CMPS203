047936508435895dfc14dabd814dff3a49df7d53
Tien Ho <thuytien140894@gmail.com>
2018-04-30 00:11:38 -0700

    Fixed formatting.

--- src/GlobalState.hs
+++ src/GlobalState.hs
@@ -1,21 +1,21 @@
 module GlobalState where 
 
-  import Syntax 
+    import Syntax 
 
-  import Data.Map (Map, toList)
-  import qualified Data.Map as Map
+    import Data.Map (Map, toList)
+    import qualified Data.Map as Map
 
-  newtype State = State (Map String Int)
-    deriving (Eq, Show)
+    newtype State = State (Map String Int)
+        deriving (Eq, Show)
 
-  -- create an empty state
-  emptyState :: State
-  emptyState = State Map.empty
+    -- create an empty state
+    emptyState :: State
+    emptyState = State Map.empty
 
-  -- look up the value for a variable
-  lookUp :: State -> String -> Maybe Int 
-  lookUp (State s) x = Map.lookup x s
+    -- look up the value for a variable
+    lookUp :: State -> String -> Maybe Int 
+    lookUp (State s) x = Map.lookup x s
 
-  -- insert a new variable or update the value for the existing variable
-  update :: State -> String -> Int -> State
-  update (State s) x n = State $ Map.insert x n s
+    -- insert a new variable or update the value for the existing variable
+    update :: State -> String -> Int -> State
+    update (State s) x n = State $ Map.insert x n s
--- test/BoolEvalSpec.hs
+++ test/BoolEvalSpec.hs
@@ -1,57 +1,57 @@
 module BoolEvalSpec where
 
-  import Syntax
-  import Evaluator (bEval)
-  import GlobalState
+    import Syntax
+    import Evaluator (bEval)
+    import GlobalState
 
-  import Test.Hspec
+    import Test.Hspec
 
-  spec :: Spec
-  spec = 
-    describe "evaluates" $ do
-      context "true" $
-        it "should be true" $
-          bEval Tru emptyState
-          `shouldBe`
-          Right Tru
+    spec :: Spec
+    spec = 
+        describe "evaluates" $ do
+            context "true" $
+                it "should be true" $
+                bEval Tru emptyState
+                `shouldBe`
+                Right Tru
 
-      context "false" $
-        it "should be false" $
-          bEval Fls emptyState
-          `shouldBe`
-          Right Fls
+            context "false" $
+                it "should be false" $
+                bEval Fls emptyState
+                `shouldBe`
+                Right Fls
 
-      context "5 == 3" $
-        it "should be false" $
-          bEval (Num 5 `Equal` Num 3) emptyState
-          `shouldBe`
-          Right Fls
+            context "5 == 3" $
+                it "should be false" $
+                bEval (Num 5 `Equal` Num 3) emptyState
+                `shouldBe`
+                Right Fls
 
-      context "3 < 10" $
-        it "should be true" $
-          bEval (Num 3 `Less` Num 10) emptyState
-          `shouldBe`
-          Right Tru
+            context "3 < 10" $
+                it "should be true" $
+                bEval (Num 3 `Less` Num 10) emptyState
+                `shouldBe`
+                Right Tru
 
-      context "!(10 == (3 + 7))" $
-        it "should be false" $
-          let b = Num 10 `Equal` (Num 3 `Add` Num 7)
-          in bEval (Not b) emptyState
-          `shouldBe`
-          Right Fls
+            context "!(10 == (3 + 7))" $
+                it "should be false" $
+                let b = Num 10 `Equal` (Num 3 `Add` Num 7)
+                in bEval (Not b) emptyState
+                `shouldBe`
+                Right Fls
 
-      context "(x == 3) && (y < 4) and [x=3,y=5]" $
-        it "should be false" $ 
-          let s  = update emptyState "x" 3
-              s' = update s "y" 5
-          in bEval ((Var "x" `Equal` Num 3) `And` (Var "y" `Less` Num 4)) s'
-          `shouldBe`
-          Right Fls
+            context "(x == 3) && (y < 4) and [x=3,y=5]" $
+                it "should be false" $ 
+                let s  = update emptyState "x" 3
+                    s' = update s "y" 5
+                in bEval ((Var "x" `Equal` Num 3) `And` (Var "y" `Less` Num 4)) s'
+                `shouldBe`
+                Right Fls
 
-      context "(x == 3) || (y < 4) and [x=3,y=5]" $
-        it "should be true" $
-          let s  = update emptyState "x" 3
-              s' = update s "y" 5
-          in bEval ((Var "x" `Equal` Num 3) `Or` (Var "y" `Less` Num 4)) s'
-          `shouldBe`
-          Right Tru
+            context "(x == 3) || (y < 4) and [x=3,y=5]" $
+                it "should be true" $
+                let s  = update emptyState "x" 3
+                    s' = update s "y" 5
+                in bEval ((Var "x" `Equal` Num 3) `Or` (Var "y" `Less` Num 4)) s'
+                `shouldBe`
+                Right Tru
